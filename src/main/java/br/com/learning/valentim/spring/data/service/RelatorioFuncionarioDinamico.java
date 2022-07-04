0983826d71736fb435956dc458c8e6251e0afa02
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-07-04 15:13:07 -0300

    Compondo Specifications

---
+++ src/main/java/br/com/learning/valentim/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -0,0 +1,60 @@
+package br.com.learning.valentim.spring.data.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.learning.valentim.spring.data.orm.Funcionario;
+import br.com.learning.valentim.spring.data.repository.FuncionarioRepository;
+import br.com.learning.valentim.spring.data.specification.SpecificationFuncionario;
+
+public class RelatorioFuncionarioDinamico {
+
+	private final FuncionarioRepository funcionarioRepository;
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
+		this.funcionarioRepository = funcionarioRepository;
+	}
+
+	public void inicial(Scanner sc) {
+		System.out.println("Digite o nome");
+		String nome = sc.next();
+
+		if (nome.equalsIgnoreCase("NULL")) {
+			nome = null;
+		}
+
+		System.out.println("Digite o cpf");
+		String cpf = sc.next();
+
+		if (cpf.equalsIgnoreCase("NULL")) {
+			cpf = null;
+		}
+
+		System.out.println("Digite o salario");
+		Double salario = sc.nextDouble();
+
+		if (salario == 0) {
+			salario = null;
+		}
+
+		System.out.println("Digite a data de contratacao");
+		String data = sc.next();
+		LocalDate dataContratacao;
+
+		if (data.equalsIgnoreCase("NULL")) {
+			dataContratacao = null;
+		} else {
+			dataContratacao = LocalDate.parse(data, formatter);
+		}
+
+		List<Funcionario> funcionarios = funcionarioRepository
+				.findAll(Specification.where(SpecificationFuncionario.nome(nome)).or(SpecificationFuncionario.cpf(cpf))
+						.or(SpecificationFuncionario.salario(salario))
+						.or(SpecificationFuncionario.dataContratacao(dataContratacao)));
+	}
+}
--- src/main/java/br/com/learning/valentim/spring/data/specification/SpecificationFuncionario.java
+++ src/main/java/br/com/learning/valentim/spring/data/specification/SpecificationFuncionario.java
@@ -1,13 +1,28 @@
 package br.com.learning.valentim.spring.data.specification;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import br.com.learning.valentim.spring.data.orm.Funcionario;
 
 public class SpecificationFuncionario {
 
-	public static Specification<Funcionario> nome(String nome){
-		return 
-				(root, criteriaApi, criteriaBuilder) -> criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+	public static Specification<Funcionario> nome(String nome) {
+		return (root, criteriaApi, criteriaBuilder) -> criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
 	}
+
+	public static Specification<Funcionario> cpf(String cpf) {
+		return (root, criteriaApi, criteriaBuilder) -> criteriaBuilder.equal(root.get("cpf"), cpf);
+	}
+
+	public static Specification<Funcionario> salario(Double salario) {
+		return (root, criteriaApi, criteriaBuilder) -> criteriaBuilder.equal(root.get("salario"), salario);
+	}
+
+	public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+		return (root, criteriaApi, criteriaBuilder) -> criteriaBuilder.equal(root.get("dataContratacao"),
+				dataContratacao);
+	}
+
 }
