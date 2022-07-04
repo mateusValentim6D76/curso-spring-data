37ad458bc788435e9845897378cd9770196a9404
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-07-04 11:57:41 -0300

    Adicionando interface JpaSpecificationExecutor

--- src/main/java/br/com/learning/valentim/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/learning/valentim/spring/data/repository/FuncionarioRepository.java
@@ -3,6 +3,7 @@ package br.com.learning.valentim.spring.data.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,8 @@ import br.com.learning.valentim.spring.data.orm.Funcionario;
 import br.com.learning.valentim.spring.data.orm.FuncionarioProjection;
 
 @Repository
-public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+public interface FuncionarioRepository
+		extends PagingAndSortingRepository<Funcionario, Integer>, JpaSpecificationExecutor<Funcionario> {
 
 	List<Funcionario> findByNome(String nome);
 
@@ -27,7 +29,7 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 	 */
 	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
-	
+
 	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
 	List<FuncionarioProjection> findFuncionarioSalario();
 
